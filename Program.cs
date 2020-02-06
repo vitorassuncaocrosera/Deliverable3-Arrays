fb242c417be77fb7183449bfb598518bb517d900
VITOR <VITOR@VITORC>
2020-02-06 17:27:52 -0500

    commit all

--- Program.cs
+++ Program.cs
@@ -6,16 +6,24 @@ namespace Deliverable3_Arrays
     {
         static void Main(string[] args)
         {
-            int size = 25; // declare size of array variable
-            int number = 1; // declare number variable
-            int[] array = new int[size]; // declare array
-
-            // for loop to iterate through each item inside array
-            for (int i = 0; i <= size - 1; i += 1) // because first i equals 0, we need to subtract 1 from size to loop through 25 items
+            try
             {
-                array[i] = number; // store number inside array
-                Console.WriteLine("Element value: " + array[i]); // print a string followed by array item
-                number += 1; // add 1 to variable number
+                int size = 25; // declare size of array variable
+                int number = 1; // declare number variable
+                int[] array = new int[size]; // declare array
+
+                // for loop to iterate through each item inside array
+                for (int i = 0; i <= size - 1; i += 1) // because first i equals 0, we need to subtract 1 from size to loop through 25 items
+                {
+                    array[i] = number; // store number inside array
+                    Console.WriteLine("Element value: " + array[i]); // print a string followed by array item
+                    number += 1; // add 1 to variable number
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Error in the try block");
+                Console.WriteLine("Press any key to exit");
             }
         }
     }
